fb73f9a35c10b5d5670c1dd22cfa73c51dcdd2a2
cameron-klales <tuf62129@temple.edu>
2015-11-10 14:05:09 -0500

    Update determine_order.c

--- determine_order.c
+++ determine_order.c
@@ -1,24 +1,24 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int determine_order(void);
+char determine_order(void);
 
-int main(void){
+char main(void){
 }
 
-int determine_order(void){
+char determine_order(void){
         int p1;
         char z;
 
         while(1){
         scanf("%c", &z);
 
-        if(z == "x" || z == "X"){
+        if(z == 'x' || z == 'X'){
                 printf("You have chosen to play as %c", z);
-                return 1;
-        }else if(z == "O" || z == "o"){
+                return 'X';
+        }else if(z == 'O' || z == 'o'){
                 printf("You have chosen to play as %c", z);
-                return 2;
+                return 'O';
         }else{
                 puts("Try again");
                 continue;
