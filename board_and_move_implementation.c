40fb126f43db80eb37ed1a3e450f75648807a6fe
cameron-klales <tuf62129@temple.edu>
2015-11-09 20:40:07 -0500

    Update board_and_move_implementation.c

--- board_and_move_implementation.c
+++ board_and_move_implementation.c
@@ -1,19 +1,20 @@
 #include<stdio.h>
 #include<stdio.h>
 
-void print_board(char board[]);
+void print_board(char board[], char c);
 void implement_user_move(int board[]);
 
 int main(void){
 char tuna[9] = {'1','2','3','4','5','6','7','8','9'};
-print_board(tuna[]);
+char c = d;
+print_board(tuna[],c);
 
 return;
 
 }
 
 
-void print_board( int board[]){
+void print_board( char board[]){
 
 
 
@@ -38,15 +39,16 @@ int getspace( void ){
 return 0;
 }
 
-void implement_user_move(char board[ ]){
+void implement_user_move(char board[ ], char c){
         int bn;
         while(1){
                 bn = getspace();
                 if (board[bn] != ' '){
                         printf("%d is already claimed, try again.", bn);
+                        continue;
                 }else{
                         printf("you have claimed %d",bn);
-                        board[bn]=
+                        board[bn] = c;
                 }
                 }
         scanf("Enter the box you would like to claim %d", &bn);
