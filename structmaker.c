2a1ab410f713d6150721d727ce6e302235381b6f
cameron-klales <tuf62129@temple.edu>
2015-11-18 15:59:37 -0500

    Update structmaker.c

--- structmaker.c
+++ structmaker.c
@@ -33,6 +33,26 @@ int main(){
     feb[i].openings = february_2015_opening_average[i];
     feb[i].closings = february_2015_closing[i];
     feb[i].volume = february_2015_volume[i];
-    strncpy ( feb[i].date, february_2015_date_recorded[i], sizeof(str2) );
+    strncpy ( feb[i].date, february_2015_date_recorded[i], sizeof(february_2015_date_recorded[i]) );
+  }
+  
+  j = sizeof(february_2015_opening_average[])/8;
+  scruct marinfo mar[j];
+  
+  for(i=0,i<j,i++){
+    mar[i].openings = march_2015_opening_average[i];
+    mar[i].closings = march_2015_closing[i];
+    mar[i].volume = march_2015_volume[i];
+    strncpy ( mar[i].date, march_2015_date_recorded[i], sizeof(march_2015_date_recorded[i]) );
+  }
+  
+  j = sizeof(january_2015_opening_average[])/8;
+  scruct janinfo jan[j];
+  
+  for(i=0,i<j,i++){
+    jan[i].openings = january_2015_opening_average[i];
+    jan[i].closings = january_2015_closing[i];
+    jan[i].volume = january_2015_volume[i];
+    strncpy ( jan[i].date, january_2015_date_recorded[i], sizeof(january_2015_date_recorded[i]) );
   }
 }
