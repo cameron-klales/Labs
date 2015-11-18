7594e84aee7d38ec2de8531b8e252eae59d2a5f9
cameron-klales <tuf62129@temple.edu>
2015-11-18 18:06:53 -0500

    Update printfunction.c

--- printfunction.c
+++ printfunction.c
@@ -1,1 +1,5 @@
-printfunctio
+void printfunction(double, open, close, volume, mtd, ytd char date){
+  printf("%s", date;);
+  printf("Open      Close     Volume      Month to Date     Year To Date\n");
+  printf("%lf       %lf       %lf         %lf               %lf           ", open, close, volume, mtd, ytd);
+}
