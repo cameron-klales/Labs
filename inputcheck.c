56b931c29a3ded5dd05766e1c9f6413e32d4a057
cameron-klales <tuf62129@temple.edu>
2015-11-18 17:39:37 -0500

    Update inputcheck.c

--- inputcheck.c
+++ inputcheck.c
@@ -3,14 +3,49 @@
 #include<stdlib.h>
 
 
-void check();
+int check(void){
   int i;
+  int j;
+  int k = 0;
   char input[10];
-  for(//dolater//)
-    puts("Yes");
-    gets(input);
-    if(strncasecmp(
-      i++;
-      continue;
-    else{
-      
+  
+  while(1){
+    
+  gets(input);
+    j = sizeof(february_2015_volume[])/sizeof(february_2015_volume[0]);
+    
+    for(i=0,i<j,i++){
+      puts("Yes");
+      if(strncasecmp(input,february_2015_date_recorded[i],strlen(february_2015_date_recorded[i])==0)
+        k++;
+        continue;
+      }else{
+        return i;
+        break;
+      };
+    
+    j = sizeof(february_2015_opening_average[])/sizeof(february_2015_opening_average[0]);
+    
+    for(i, i<j, i++){
+      puts("Yes");
+      if(strncasecmp(input, february_2015_date_recorded[i], strlen(february_2015_date_recorded[i])==0){
+        k++
+        continue;
+      }else{
+        return i;
+        break;
+    };
+        
+    j = sizeof(march_2015_closing[])/sizeof(march_2015_closing[0]);
+    
+    for(i, i<j, i++){
+      puts("Yes");
+      if(strncasecmp(input,march_2015_date_recorded[i],strlen(data[i])==0)
+        continue;
+      }else{
+        return i;
+        break;
+    puts("lol no")
+    };
+}
+}
