bc54b34390a0288313218a45c235513d67c7b061
cameron-klales <tuf62129@temple.edu>
2015-11-18 18:29:08 -0500

    Update combinearrays.c

--- combinearrays.c
+++ combinearrays.c
@@ -23,3 +23,29 @@ void numarrayscombine(double ray1, double ray2, double ray3, double rayfinal){
   }
   return;
 }
+
+void datearrayscombine(char ray1, char ray2, char ray3, char rayfinal){
+  int i;
+  int j;
+  int k=0;
+  
+  j = sizeof(ray1)/sizeof(ray1[0]);
+  
+  for(i=0,i<j,i++){
+    rayfinal[k] = ray1[i];
+    k++
+  }
+  j = sizeof(ray2)/sizeof(ray2[0]);
+  
+  for(i=0,i<j,i++){
+    rayfinal[k] = ray2[i];
+    k++
+  }
+  j = sizeof(ray3)/sizeof(ray3[0]);
+  
+   for(i=0,i<j,i++){
+    rayfinal[k] = ray3[i];
+    k++
+  }
+  return;
+}
