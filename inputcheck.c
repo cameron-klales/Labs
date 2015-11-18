7e561827e2cf9c6098d13712e872a90c9bf7e4af
cameron-klales <tuf62129@temple.edu>
2015-11-18 16:53:51 -0500

    Update inputcheck.c

--- inputcheck.c
+++ inputcheck.c
@@ -1,3 +1,16 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+
+
+void check();
+  int i;
+  char input[10];
+  for(//dolater//)
+    puts("Yes");
+    gets(input);
+    if(strncasecmp(
+      i++;
+      continue;
+    else{
+      
