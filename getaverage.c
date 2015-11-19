1af56057c647d3fe9ee5fcaecdec14b9f30677aa
cameron-klales <tuf62129@temple.edu>
2015-11-18 19:06:45 -0500

    Update getaverage.c

--- getaverage.c
+++ getaverage.c
@@ -1,6 +1,13 @@
-double getaverage(double rayt[)
+double getaverage(double rayt[]){
   int i;
   int j;
   double k;
+  double m;
+  j = (sizeof(rayt)/double(rayt[0]))
   
+  for(i=0, i<j,i++){
+    k += rayt(i)
+  }
+  m = k/j;
+  return m;
 }
